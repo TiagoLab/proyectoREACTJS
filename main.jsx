454a2a67c2206c90f369837067d281744b47397b
TiagoLab <tlabriola@frba.utn.edu.ar>
2025-10-24 19:30:30 -0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,2 +1,17 @@
-429: Too Many Requests
-For more on scraping GitHub and how it may affect your rights, please review our Terms of Service (https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).
+// Punto de entrada de la app — modificado por Tiago (Octubre 2025)
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import AppPrincipal from './AppPrincipal.jsx'
+import { BrowserRouter } from 'react-router-dom'
+
+const rootReact = createRoot(document.getElementById('root'))
+
+rootReact.render(
+  <StrictMode>
+    <BrowserRouter>
+      <AppPrincipal />
+    </BrowserRouter>
+  </StrictMode>
+)
